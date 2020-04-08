92e4fc25b84079b6fe0e02cc91757d65f0ee5028
Srinidhi-SR <60979099+Srinidhi-SR@users.noreply.github.com>
2020-04-08 20:51:05 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,16 @@
+class Form{
+    constructor (){
+
+    }
+    display(){
+    var title = createElement('h2');
+    title.html ("car racing game");
+    tiltle.position(130,0);
+    var Input = createInput("name");
+    var Button = createButton("play");
+    Input.position(130,160);
+    Button.position(250,200);
+    
+
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,16 @@
+var canvas;
+var gamestate = 0;
+var playerCount;
+var database;
+var form;
+var player;
+var game;
+
+function setup(){
+  canvas =createcanvas(800,400);
+  database = firebase.database();
+}
+
+function draw(){
+  
+}
